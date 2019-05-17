d3e3c720856f2e68025a324547f6cf50f792f2fe
riteshf <firodiya.ritesh@gmail.com>
2019-05-17 11:36:15 +0530

    salesPerson Filter added

--- src/components/PersonFilter/index.jsx
+++ src/components/PersonFilter/index.jsx
@@ -1,21 +1,37 @@
 import "./style.css";
 import React, { useState } from "react";
 
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 function PersonFilter(props) {
   const [employeeName, setEmployeeName] = useState(props.employees[0]);
 
   return (
     <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
-      <div className="mdl-tabs__tab-bar timeframe-tabs">
-      
-      <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select getmdl-select__fix-height"  style={{width: '150px'}}>
-        <input type="text" value="" className="mdl-textfield__input" id="sample6" readOnly />
-        <input type="hidden" value="" name="sample6" />
-        <i className="mdl-icon-toggle__label material-icons"></i>
-        <ul htmlFor="sample6" className="mdl-menu mdl-menu--bottom-left mdl-js-menu">
-        {props.employees.map((employee,i ) => <li key={i}  className="mdl-menu__item" data-val={employee}>{employee}</li>)}
-        </ul>
-        </div>
+      <div className="mdl-tabs__tab-bar personFilter-tabs">
+        <form className={props.root} autoComplete="off">
+          <FormControl className={props.formControl}>
+            <InputLabel htmlFor="aemployee-name">Sales Person</InputLabel>
+            <Select
+              style={{ width: "150px", textAlign: 'center' }}
+              value={employeeName}
+              onChange={() => setEmployeeName(employeeName)}
+              inputProps={{
+                name: "All Sales Person",
+                id: "employee-name"
+              }}
+            >
+              {props.employees.map((emplyee, id) => (
+                <MenuItem key={id} value={emplyee}>
+                  {emplyee}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </form>
       </div>
     </div>
   );
